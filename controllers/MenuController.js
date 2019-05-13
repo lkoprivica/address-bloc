49445d0e87bfd0526fdf2c4adb43174da647c505
Lindsay Koprivica <lindsayeplin@gmail.com>
2019-05-12 20:15:11 -0600

    first commit

---
+++ controllers/MenuController.js
@@ -0,0 +1,68 @@
+const inquirer = require('inquirer');
+
+module.exports = class MenuController {
+  constructor(){
+    this.mainMenuQuestions = [
+      {
+        type: "list",
+          name: "mainMenuChoice",
+          message: "Please choose from an option below: ",
+          choices: [
+            "Add new contact",
+            "Get the date",
+            "Exit"
+        ]
+      }
+    ];
+    this.contacts = [];
+
+  }
+
+  main(){
+    console.log(`Welcome to AddressBloc!`);
+     inquirer.prompt(this.mainMenuQuestions).then((response) => {
+       switch(response.mainMenuChoice){
+         case "Add new contact":
+            this.addContact();
+            break;
+          case "Get the Date":
+            this.getDate();
+            break;
+          case "Exit":
+            this.exit();
+            default:
+              console.log("Invalid input");
+              this.main();
+       }
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+   }
+
+  clear(){
+    //console.log('\x1Bc');
+  }
+
+  addContact(){
+    this.clear();
+    console.log('addContact called');
+    this.main();
+  }
+
+  getDate(){
+    this.clear();
+    let date = new Date();
+    let n = date.toDateString();
+    let time = date.toLocaleTimeString();
+
+    console.log('date:', n);
+    console.log('time:', time);
+    this.main();
+  }
+
+  exit(){
+    console.log("Thanks for using AddressBloc!");
+    process.exit();
+  }
+}
